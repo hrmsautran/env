1388619f5754bd130d918505ce05414738d84108
Hermes Autran <hermes.autran@gmail.com>
2019-04-16 11:18:55 -0400

    add unzip and tintedarc theme.

--- configure.sh
+++ configure.sh
@@ -18,7 +18,7 @@ install_xfce4() {
   
   PACKAGES="xorg-server xorg-xinit xorg-twm xorg-xclock xterm xfce4 lightdm "
   PACKAGES+="xf86-input-mouse xf86-input-keyboard xf86-video-vesa "
-  PACKAGES+="wget htop git vim zsh bash-completion ctags docker unrar "
+  PACKAGES+="wget htop git vim zsh bash-completion ctags docker unrar unzip "
   # PACKAGES+="qemu-kvm qemu virt-manager virt-viewer libvirt-bin "
   #PACKAGES+="chromium firefox opera vlc clementine"
 
@@ -50,10 +50,16 @@ configure_desktop() {
   sudo systemctl enable lightdm
   
   # Download Rele Theme
-  curl -o /tmp/rele.tar.bz2 https://dl.opendesktop.org/api/files/downloadfile/id/1462392025/s/947eec5cfb08c2bb291190370343f799/t/1527263464/u/60272/77260-rele-xfce4.tar.bz2
-  tar -xjvf /tmp/rele.tar.bz2
-  sudo cp -R /tmp/Rele /usr/share/themes/Rele
+  # curl -o /tmp/rele.tar.bz2 https://dl.opendesktop.org/api/files/downloadfile/id/1462392025/s/947eec5cfb08c2bb291190370343f799/t/1527263464/u/60272/77260-rele-xfce4.tar.bz2
+  # tar -xjvf /tmp/rele.tar.bz2
+  # sudo cp -R /tmp/Rele /usr/share/themes/Rele
 
+  curl -o /tmp/tintedarc.zip https://github.com/nizarmah/tintedarc/archive/master.zip
+  unzip /tmp/tintedarc.zip -d /tmp/tintedarc
+  rm -rf /tmp/tintedarc.zip
+  sudo -s ./desktop-mode --setup
+  desktop-mode --night
+  
   # download image backgroud
   # xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s /temp/background.jpg
 }
